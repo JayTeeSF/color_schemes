dcefa5efba6dfc063f3046ccd9da3bc6a0d31447
JayTeeSF <jaytee@jayteesf.com>
2024-01-02 12:59:46 -0800

    ch4 iter4

--- script.js
+++ script.js
@@ -5,6 +5,9 @@ document.addEventListener('DOMContentLoaded', () => {
     const clearCookiesButton = document.getElementById("clear-cookies");
     const renderButton = document.getElementById("render-button");
 
+    populateFontDropdown();
+    populateColorSchemeDropdown();
+
     renderButton.addEventListener("click", () => {
         const selectedFont = fontDropdown.value;
         const selectedColorScheme = colorSchemeDropdown.value;
@@ -14,30 +17,48 @@ document.addEventListener('DOMContentLoaded', () => {
     clearCookiesButton.addEventListener("click", clearCookies);
 });
 
+function populateFontDropdown() {
+    const fonts = ["Arial", "Verdana", "Helvetica", "Tahoma", "Trebuchet MS", "Times New Roman", "Georgia", "Garamond", "Courier New", "Brush Script MT"];
+    fonts.forEach(font => {
+        let option = new Option(font, font);
+        fontDropdown.add(option);
+    });
+}
+
+function populateColorSchemeDropdown() {
+    const colorSchemes = {
+        'Cerulean Lime Crimson': ['#007bff', '#A2C95C', '#F05134'],
+        'Royal Emerald Crimson': ['#4169E1', '#2ECC71', '#E74C3C'],
+        'Midnight Olive Silver': ['#2F4F4F', '#66A350', '#CCCCCC'],
+        'Twitter Teal Light Grey': ['#3498DB', '#38A3A5', '#F1F1F1']
+    };
+
+    Object.keys(colorSchemes).forEach(scheme => {
+        let option = new Option(scheme, scheme);
+        colorSchemeDropdown.add(option);
+    });
+}
+
 function applyPreferences(font, colorScheme) {
     document.body.style.fontFamily = font;
     applyColorScheme(colorScheme);
 }
 
 function applyColorScheme(scheme) {
-    let colors = {
-        light: {
-            primary: "#007bff",
-            secondary: "#A2C95C",
-            accent: "#F05134"
-        },
-        dark: {
-            primary: "#2F4F4F",
-            secondary: "#66A350",
-            accent: "#F1F1F1"
-        }
+    const colorSchemes = {
+        'Cerulean Lime Crimson': ['#007bff', '#A2C95C', '#F05134'],
+        'Royal Emerald Crimson': ['#4169E1', '#2ECC71', '#E74C3C'],
+        'Midnight Olive Silver': ['#2F4F4F', '#66A350', '#CCCCCC'],
+        'Twitter Teal Light Grey': ['#3498DB', '#38A3A5', '#F1F1F1']
     };
 
-    let schemeColors = colors[scheme];
+    let colors = colorSchemes[scheme];
     
-    document.documentElement.style.setProperty('--primary-color', schemeColors.primary);
-    document.documentElement.style.setProperty('--secondary-color', schemeColors.secondary);
-    document.documentElement.style.setProperty('--accent-color', schemeColors.accent);
+    if(colors) {
+        document.documentElement.style.setProperty('--primary-color', colors[0]);
+        document.documentElement.style.setProperty('--secondary-color', colors[1]);
+        document.documentElement.style.setProperty('--accent-color', colors[2]);
+    }
 }
 
 function clearCookies() {
